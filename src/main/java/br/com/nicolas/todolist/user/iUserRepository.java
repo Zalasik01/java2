16278044907a481346115db9f4eb11dc473b91c2
Nicolas <nicolas.souza@altimus.com.br>
2024-08-28 13:13:13 -0300

    Validador de usuario

---
+++ src/main/java/br/com/nicolas/todolist/user/iUserRepository.java
@@ -0,0 +1,10 @@
+package br.com.nicolas.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface iUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+    
+}
