a0d383bdeb6eb76e935ea7bbbcf4efc661cb2606
Nicolas <nicolas.souza@altimus.com.br>
2024-08-28 13:27:35 -0300

    Criação do banco de task

---
+++ src/main/java/br/com/nicolas/todolist/task/TaskModel.java
@@ -0,0 +1,45 @@
+package br.com.nicolas.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+
+@Data
+@Entity(name = "tb_task")
+public class TaskModel {
+    
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "descricao")
+    private String description;
+
+    @Column(name = "titulo", length = 50)
+    private String title;
+
+    @Column(name = "inicio")
+    private LocalDateTime startAt;
+
+    @Column(name = "fim")
+    private LocalDateTime endAt;
+
+    @Column(name = "prioridade")
+    private String priority;
+
+    @Column(name = "id_usuario")
+    private UUID idUser;
+
+    @CreationTimestamp
+    @Column(name = "criacao")
+    private LocalDateTime creatAt;    
+}
